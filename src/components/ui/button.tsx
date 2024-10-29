d091c938dcb90d3060484895262ced8484002357
acharlop <avicharlop@gmail.com>
2024-10-29 21:31:06 +0100

    buttons with stories

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,33 +1,39 @@
 import {Slot} from '@radix-ui/react-slot'
 import {cva, type VariantProps} from 'class-variance-authority'
-import {Loader2} from 'lucide-react'
+import {ChevronRight, Loader2} from 'lucide-react'
 import * as React from 'react'
 
 import {cn} from '~/lib/utils'
 
 const buttonVariants = cva(
-  'relative inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'relative inline-flex items-center justify-center whitespace-nowrap text-center transition-colors focus-visible:outline-none focus-visible:ring-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        default: 'bg-primary text-dark hover:bg-yellow-500 active:bg-primary/90',
+        secondary: 'bg-primary-dark text-white hover:bg-primary-dark/80 active:bg-primary-dark/90',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/80 active:bg-destructive/90',
+        outline: 'border border-foreground hover:bg-gray-100/25 active:bg-gray-200/25',
+        ghost: 'hover:bg-foreground/10 active:bg-foreground/20',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: 'pl-7 pr-6 py-3.5 text-lg font-medium gap-2 rounded-2xl',
+        sm: 'pl-4 pr-3 py-2 text-base font-medium gap-1.5 rounded-xl',
+        icon: 'h-12 w-12 rounded-2xl',
       },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
     },
+    compoundVariants: [
+      {
+        variant: 'link',
+        size: ['default', 'sm', 'icon'],
+        className: 'p-[initial]',
+      },
+    ],
   }
 )
 
@@ -36,25 +42,25 @@ export type ButtonVariants = VariantProps<typeof buttonVariants>
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {
   asChild?: boolean
   loading?: boolean
+  hideIcon?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({className, variant, size, asChild = false, loading = false, children, ...props}, ref) => {
+  (
+    {className, variant, size, asChild = false, loading = false, hideIcon: hideIconProp = false, children, ...props},
+    ref
+  ) => {
     const Comp = asChild ? Slot : 'button'
+    const hideIcon = hideIconProp || variant === 'link' || size === 'icon'
     return (
       <Comp
-        className={cn(buttonVariants({variant, size, className}), loading && 'pointer-events-none')}
+        className={cn(buttonVariants({variant, size, className}), loading && 'pointer-events-none text-opacity-50')}
         ref={ref}
         {...props}
       >
-        {loading ? (
-          <>
-            <Loader2 className='absolute mx-auto h-10 w-4 animate-spin' />
-            <span className='contents opacity-0'>{children}</span>
-          </>
-        ) : (
-          children
-        )}
+        {children}
+        {!hideIcon && !loading && <ChevronRight className={size === 'sm' ? 'h-5 w-5' : 'h-6 w-6'} />}
+        {loading && <Loader2 className={cn(size === 'sm' ? 'h-5 w-5' : 'h-6 w-6', 'animate-spin')} />}
       </Comp>
     )
   }
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react'
 import {fn} from '@storybook/test'
+import {MenuIcon} from 'lucide-react'
 
 import {Button, type ButtonVariants} from '../components/ui/button'
 
@@ -13,7 +14,8 @@ const meta = {
   argTypes: {
     children: {control: 'text'},
     asChild: {control: {disable: true}},
-    size: {control: 'select'},
+    size: {control: 'select', options: ['default', 'sm', 'icon']},
+    variant: {control: 'select', options: ['default', 'outline', 'secondary', 'ghost', 'link', 'destructive']},
   },
   args: {onClick: fn()},
 } satisfies Meta<typeof Button>
@@ -27,10 +29,13 @@ export const Primary: Story = {
     children: 'Button',
     variant: 'default',
     size: 'default',
+    disabled: false,
+    loading: false,
+    hideIcon: false,
   },
 }
 
-const variants: ButtonVariants['variant'][] = ['default', 'outline', 'secondary', 'ghost', 'link', 'destructive']
+const variants: ButtonVariants['variant'][] = ['default', 'outline', 'secondary', 'ghost', 'destructive']
 
 export const All: Story = {
   args: {
@@ -39,10 +44,23 @@ export const All: Story = {
     disabled: false,
   },
   render: (args) => (
-    <div className='grid grid-cols-3 gap-4'>
+    <div className='grid grid-cols-3 items-end gap-4'>
       {variants.map((variant) => (
         <Button {...args} key={variant} variant={variant} />
       ))}
     </div>
   ),
+}
+
+export const Icon: Story = {
+  argTypes: {
+    children: {control: {disable: true}},
+  },
+  args: {
+    children: <MenuIcon className='h-12 w-12' />,
+    variant: 'outline',
+    size: 'icon',
+    disabled: false,
+    hideIcon: false,
+  },
 }
