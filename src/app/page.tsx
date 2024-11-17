b2207994bc65701ddb7de7aea36d1a8b7079eedc
acharlop <avicharlop@gmail.com>
2024-11-17 22:40:15 +0100

    update login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
 import {HydrateClient} from '~/trpc/server'
 
 export default async function HomePage() {
   return (
     <HydrateClient>
-      <h1 className='font-sans text-3xl'>home</h1>
-      <h1 className='font-title text-3xl'>home</h1>
+      <h1 className='font-sans text-3xl'>Login</h1>
+      <Link href='/api/auth/login'>Login</Link>
     </HydrateClient>
   )
 }
