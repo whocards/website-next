942737b95f451018dd600034a069fb496ac61862
acharlop <avicharlop@gmail.com>
2024-11-20 16:14:36 +0100

    implements #13 /me page with profile form and purchase history

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,8 @@
-import {LoginForm} from '~/components/login-form'
+import {LoginForm} from '~/components/forms/login-form'
 
 export default function Page() {
   return (
-    <div className='flex h-screen w-full items-center justify-center px-4'>
+    <div className='container mx-auto flex h-screen w-full items-center justify-center px-4'>
       <LoginForm />
     </div>
   )
--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,21 +1,39 @@
 import Link from 'next/link'
+import {redirect} from 'next/navigation'
+import {ProfileForm} from '~/components/forms/profile-form'
 import {Button} from '~/components/ui/button'
+import {PurchasesTable} from '~/features/purchases/PurchasesTable'
 import {hasPermission} from '~/lib/permissions'
 import {auth} from '~/server/auth'
+import {api} from '~/trpc/server'
 
 export default async function ProfilePage() {
   const session = await auth()
 
-  const canAccessAdmin = hasPermission(session?.user, 'portal', 'view')
+  if (!session?.user) {
+    redirect('/login')
+  }
+
+  const isAdmin = hasPermission(session?.user, 'portal', 'view')
+  const myPurchases = await api.purchases.getMine()
 
   return (
-    <>
-      <div>Profile Page more to come</div>
-      {canAccessAdmin && (
-        <Button asChild>
-          <Link href='/wc'>Admin page</Link>
-        </Button>
+    <div className='container mx-auto flex flex-col gap-8 p-6'>
+      <h1 className='text-3xl font-bold'>Profile</h1>
+      {isAdmin && (
+        <p>
+          Role: Admin{' '}
+          <Button asChild variant='link'>
+            <Link href='/wc'>Visit Admin page</Link>
+          </Button>
+        </p>
       )}
-    </>
+      <ProfileForm user={session?.user} />
+
+      <div>
+        <h2 className='mb-4 text-2xl font-semibold'>Purchase History</h2>
+        <PurchasesTable purchases={myPurchases} />
+      </div>
+    </div>
   )
 }
