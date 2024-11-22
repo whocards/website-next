2710e5c2bfc3f6aadebf7a74195c0cc2afcc2b84
Avi Charlop <avicharlop@gmail.com>
2024-11-22 01:12:27 +0100

    preview auth (#26)
    * debugging for fix #24
    
    * add redirect to auth config
    
    * add some logging
    
    * use provider redirectProxyUrl
    
    * remove debugging
    
    * woops
    
    * redirect from /login when authenticated

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,6 +2,7 @@ import {DrizzleAdapter} from '@auth/drizzle-adapter'
 import {type DefaultSession, type NextAuthConfig} from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import {env} from '~/env'
 
 import {db} from '~/server/db'
 import {authAccounts, authSessions, authUsers, authVerificationTokens, type UserRole} from '~/server/db/schema'
@@ -32,11 +33,17 @@ declare module 'next-auth' {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  debug: true,
   pages: {
     signIn: '/login',
   },
-  providers: [GithubProvider, GoogleProvider],
+  providers: [
+    GithubProvider({
+      redirectProxyUrl: env.AUTH_REDIRECT_PROXY_URL,
+    }),
+    GoogleProvider({
+      redirectProxyUrl: env.AUTH_REDIRECT_PROXY_URL,
+    }),
+  ],
   adapter: DrizzleAdapter(db, {
     usersTable: authUsers,
     accountsTable: authAccounts,
@@ -44,16 +51,7 @@ export const authConfig = {
     verificationTokensTable: authVerificationTokens,
   }),
   callbacks: {
-    redirect: ({url, baseUrl}) => {
-      console.log('[NextAuth] Redirect callback invoked:', {url, baseUrl})
-      return url.startsWith(baseUrl) ? url : baseUrl
-    },
-    async signIn({user, account, profile}) {
-      console.log('[NextAuth] SignIn callback invoked:', {user, account, profile})
-      return true // Ensure you don't reject logins unintentionally
-    },
     session: ({session, user}) => {
-      console.log('[NextAuth] Session callback invoked:', {session, user})
       return {
         ...session,
         user: {
