bb9647b03048af928cc85833a27dbd81817a96d6
acharlop <avicharlop@gmail.com>
2024-11-22 21:08:46 +0100

    implements #28 resend emails for login

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -1,13 +1,11 @@
-'use client'
-
 import {Button} from '~/components/ui/button'
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '~/components/ui/card'
 import {Input} from '~/components/ui/input'
 import {Label} from '~/components/ui/label'
-import {Separator} from '../ui/separator'
+import {Separator} from '~/components/ui/separator'
 import Github from '~/assets/icons/github.svg'
 import Google from '~/assets/icons/google.svg'
-import {signIn} from 'next-auth/react'
+import {signIn} from '~/server/auth'
 
 export function LoginForm() {
   return (
@@ -18,33 +16,45 @@ export function LoginForm() {
       </CardHeader>
       <CardContent>
         <div className='grid gap-4'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input id='email' type='email' placeholder='m@example.com' required disabled />
-          </div>
-          <Button type='submit' className='w-full' disabled>
-            Login
-          </Button>
+          <form
+            className='contents'
+            action={async (formData) => {
+              'use server'
+              await signIn('resend', formData)
+            }}
+          >
+            <div className='grid gap-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input id='email' type='email' name='email' placeholder='me@example.com' required />
+            </div>
+            <Button type='submit' className='w-full'>
+              Login
+            </Button>
+          </form>
           <Separator className='mx-auto my-2 h-0.5 w-[50%] bg-white/50' />
           <div className='flex gap-4'>
-            <Button
-              variant='outline'
-              className='w-full'
-              onClick={() => {
-                void signIn('google', {redirectTo: '/me'})
+            <form
+              className='contents'
+              action={async () => {
+                'use server'
+                await signIn('google', {redirectTo: '/me'})
               }}
             >
-              <Google className='size-5 fill-white' />
-            </Button>
-            <Button
-              variant='outline'
-              className='w-full'
-              onClick={() => {
-                void signIn('github', {redirectTo: '/me'})
+              <Button variant='outline' type='submit' className='w-full'>
+                <Google className='size-5 fill-white' />
+              </Button>
+            </form>
+            <form
+              className='contents'
+              action={async () => {
+                'use server'
+                await signIn('github', {redirectTo: '/me'})
               }}
             >
-              <Github className='size-5 fill-white' />
-            </Button>
+              <Button variant='outline' className='w-full' type='submit'>
+                <Github className='size-5 fill-white' />
+              </Button>
+            </form>
           </div>
         </div>
       </CardContent>
--- src/env.ts
+++ src/env.ts
@@ -13,8 +13,10 @@ export const env = createEnv({
     AUTH_GOOGLE_ID: z.string(),
     AUTH_GOOGLE_SECRET: z.string(),
     AUTH_REDIRECT_PROXY_URL: z.string().url().optional(),
+    AUTH_RESEND_KEY: z.string(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    IS_DEV: z.boolean(),
   },
 
   /**
@@ -37,8 +39,10 @@ export const env = createEnv({
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
     AUTH_REDIRECT_PROXY_URL: process.env.AUTH_REDIRECT_PROXY_URL,
+    AUTH_RESEND_KEY: process.env.AUTH_RESEND_KEY,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    IS_DEV: process.env.NODE_ENV === 'development',
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
