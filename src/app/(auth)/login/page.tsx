09a2158134a5288068817472c5f169be561a3b30
acharlop <avicharlop@gmail.com>
2024-11-22 01:11:00 +0100

    redirect from /login when authenticated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,19 @@
 import {LoginForm} from '~/components/forms/login-form'
 import type {Metadata} from 'next'
+import {auth} from '~/server/auth'
+import {redirect} from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'WhoCards | Login',
   description: 'WhoCards Login',
 }
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+  if (session) {
+    redirect('/me')
+  }
+
   return (
     <div className='container mx-auto flex h-screen w-full items-center justify-center px-4'>
       <LoginForm />
