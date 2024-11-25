64207d9c082a373ec01736c074410a74ce66b69a
acharlop <avicharlop@gmail.com>
2024-11-25 13:57:49 +0100

    analytics provider initialize without useEffect

--- src/components/providers/analytics.tsx
+++ src/components/providers/analytics.tsx
@@ -2,23 +2,18 @@
 
 import posthog from 'posthog-js'
 import {PostHogProvider} from 'posthog-js/react'
-import {useEffect} from 'react'
 import {env} from '~/env'
 
 if (typeof window !== 'undefined' && env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: '/ingest',
+    person_profiles: 'always',
+    capture_pageview: false, // We capture pageviews in the AnalyticsPageView component
+    capture_pageleave: true,
+  })
 }
 
 export function AnalyticsProvider({children}: {children: React.ReactNode}) {
-  useEffect(() => {
-    if (env.NEXT_PUBLIC_POSTHOG_DISABLED) return
-    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: '/ingest',
-      person_profiles: 'always',
-      capture_pageview: false, // We capture pageviews in the AnalyticsPageView component
-      capture_pageleave: true,
-    })
-  }, [])
-
   if (!env.NEXT_PUBLIC_POSTHOG_DISABLED) return <>{children}</>
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
