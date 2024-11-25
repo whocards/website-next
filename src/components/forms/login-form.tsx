064bee789081048a08e7bd69b38cca23d163fb6a
acharlop <avicharlop@gmail.com>
2024-11-25 16:01:37 +0100

    use server actions from login page for signin

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {Button} from '~/components/ui/button'
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '~/components/ui/card'
 import {Input} from '~/components/ui/input'
@@ -5,9 +7,22 @@ import {Label} from '~/components/ui/label'
 import {Separator} from '~/components/ui/separator'
 import Github from '~/assets/icons/github.svg'
 import Google from '~/assets/icons/google.svg'
-import {signIn} from '~/server/auth'
+import {useSessionUser} from '~/hooks/use-session-user'
+import {redirect} from 'next/navigation'
 
-export function LoginForm() {
+type Props = {
+  signInResend: (formData: FormData) => Promise<void>
+  signInGoogle: () => Promise<void>
+  signInGithub: () => Promise<void>
+}
+
+export const LoginForm = ({signInResend, signInGoogle, signInGithub}: Props) => {
+  const session = useSessionUser()
+
+  if (session) {
+    redirect('/me')
+  }
+
   return (
     <Card className='mx-auto max-w-sm'>
       <CardHeader>
@@ -16,13 +31,7 @@ export function LoginForm() {
       </CardHeader>
       <CardContent>
         <div className='grid gap-4'>
-          <form
-            className='contents'
-            action={async (formData) => {
-              'use server'
-              await signIn('resend', formData)
-            }}
-          >
+          <form className='contents' action={signInResend}>
             <div className='grid gap-2'>
               <Label htmlFor='email'>Email</Label>
               <Input id='email' type='email' name='email' placeholder='me@example.com' required />
@@ -33,24 +42,12 @@ export function LoginForm() {
           </form>
           <Separator className='mx-auto my-2 h-0.5 w-[50%] bg-white/50' />
           <div className='flex gap-4'>
-            <form
-              className='contents'
-              action={async () => {
-                'use server'
-                await signIn('google', {redirectTo: '/me'})
-              }}
-            >
+            <form className='contents' action={signInGoogle}>
               <Button variant='outline' type='submit' className='w-full'>
                 <Google className='size-5 fill-white' />
               </Button>
             </form>
-            <form
-              className='contents'
-              action={async () => {
-                'use server'
-                await signIn('github', {redirectTo: '/me'})
-              }}
-            >
+            <form className='contents' action={signInGithub}>
               <Button variant='outline' className='w-full' type='submit'>
                 <Github className='size-5 fill-white' />
               </Button>
